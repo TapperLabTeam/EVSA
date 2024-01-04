d15be48863eac6dec07a6c8c8d8784180bab7549
TapperLab <119892368+TapperLab@users.noreply.github.com>
2024-01-04 12:53:49 -0500

    Update evsa_blocks.py

--- evsa_blocks.py
+++ evsa_blocks.py
@@ -13,107 +13,15 @@ event_dict = unpickle([sys.argv[4]])[0]
 data_dict = {'File Name': file_handle, 'B1 Active Time-In Pokes': 0, 'B1 Active Time-Out Pokes': 0, 'B1 Inactive Time-In Pokes': 0, 'B1 Inactive Time-Out Pokes': 0, 'B1 Total Active Pokes': 0, 'B1 Total Inactive Pokes': 0, 'B1 Puffs': 0, 'B2 Active Time-In Pokes': 0, 'B2 Active Time-Out Pokes': 0, 'B2 Inactive Time-In Pokes': 0, 'B2 Inactive Time-Out Pokes': 0, 'B2 Total Active Pokes': 0, 'B2 Total Inactive Pokes': 0, 'B2 Puffs': 0, 'B3 Active Time-In Pokes': 0, 'B3 Active Time-Out Pokes': 0, 'B3 Inactive Time-In Pokes': 0, 'B3 Inactive Time-Out Pokes': 0, 'B3 Total Active Pokes': 0, 'B3 Total Inactive Pokes': 0, 'B3 Puffs': 0, 'B4 Active Time-In Pokes': 0, 'B4 Active Time-Out Pokes': 0, 'B4 Inactive Time-In Pokes': 0, 'B4 Inactive Time-Out Pokes': 0, 'B4 Total Active Pokes': 0, 'B4 Total Inactive Pokes': 0, 'B4 Puffs': 0, 'Active Time-In Pokes': 0, 'Active Time-Out Pokes': 0, 'Total Active Pokes': 0, 'Inactive Time-In Pokes': 0, 'Inactive Time-Out Pokes': 0, 'Total Inactive Pokes': 0, 'Total Puffs': 0, 'Discrimination Index': 0}
 #Group data in 4 30 minute bins
 if event_dict['Events'] != 'NaN':
-	temp_time = -999
+	#temp_time = -999
 	ts = 0
-	time_check = 0
+	puff_time = 'NaN'
+	time_check = 'NaN'
 	for event_num, event in enumerate(event_dict['Events']):
 		ts = float(event_dict['Time (s)'][event_num])
-		time_check = ts - temp_time
-		if event == 'Active Poke':
-			if ts <= 1800: #Active Block 1
-				if time_check >= 60:
-					data_dict['B1 Active Time-In Pokes'] += 1
-					data_dict['B1 Total Active Pokes'] += 1
-					data_dict['Active Time-In Pokes'] += 1
-					data_dict['Total Active Pokes'] += 1
-					temp_time = ts
-				if time_check < 60:
-					data_dict['B1 Active Time-Out Pokes'] += 1
-					data_dict['B1 Total Active Pokes'] += 1
-					data_dict['Active Time-Out Pokes'] += 1
-					data_dict['Total Active Pokes'] += 1
-			if ts > 1800 and ts <= 3600: #Active Block 2
-				if time_check >= 60:
-					data_dict['B2 Active Time-In Pokes'] += 1
-					data_dict['B2 Total Active Pokes'] += 1
-					data_dict['Active Time-In Pokes'] += 1
-					data_dict['Total Active Pokes'] += 1
-					temp_time = ts
-				if time_check < 60:
-					data_dict['B2 Active Time-Out Pokes'] += 1
-					data_dict['B2 Total Active Pokes'] += 1
-					data_dict['Active Time-Out Pokes'] += 1
-					data_dict['Total Active Pokes'] += 1
-			if ts > 3600 and ts <= 5400: #Active Block 3
-				if time_check >= 60:
-					data_dict['B3 Active Time-In Pokes'] += 1
-					data_dict['B3 Total Active Pokes'] += 1
-					data_dict['Active Time-In Pokes'] += 1
-					data_dict['Total Active Pokes'] += 1
-					temp_time = ts
-				if time_check < 60:
-					data_dict['B3 Active Time-Out Pokes'] += 1
-					data_dict['B3 Total Active Pokes'] += 1
-					data_dict['Active Time-Out Pokes'] += 1
-					data_dict['Total Active Pokes'] += 1
-			if ts > 5400: #Active Block 4
-				if time_check >= 60:
-					data_dict['B4 Active Time-In Pokes'] += 1
-					data_dict['B4 Total Active Pokes'] += 1
-					data_dict['Active Time-In Pokes'] += 1
-					data_dict['Total Active Pokes'] += 1
-					temp_time = ts
-				if time_check < 60:
-					data_dict['B4 Active Time-Out Pokes'] += 1
-					data_dict['B4 Total Active Pokes'] += 1
-					data_dict['Active Time-Out Pokes'] += 1
-					data_dict['Total Active Pokes'] += 1
-		if event == 'Inactive Poke':
-			if ts <= 1800: #Inactive Block 1
-				if time_check >= 60:
-					data_dict['B1 Inactive Time-In Pokes'] += 1
-					data_dict['B1 Total Inactive Pokes'] += 1
-					data_dict['Inactive Time-In Pokes'] += 1
-					data_dict['Total Inactive Pokes'] += 1
-				if time_check < 60:
-					data_dict['B1 Inactive Time-Out Pokes'] += 1
-					data_dict['B1 Total Inactive Pokes'] += 1
-					data_dict['Inactive Time-Out Pokes'] += 1
-					data_dict['Total Inactive Pokes'] += 1
-			if ts > 1800 and ts <= 3600: #Inactive Block 2
-				if time_check >= 60:
-					data_dict['B2 Inactive Time-In Pokes'] += 1
-					data_dict['B2 Total Inactive Pokes'] += 1
-					data_dict['Inactive Time-In Pokes'] += 1
-					data_dict['Total Inactive Pokes'] += 1
-				if time_check < 60:
-					data_dict['B2 Inactive Time-Out Pokes'] += 1
-					data_dict['B2 Total Inactive Pokes'] += 1
-					data_dict['Inactive Time-Out Pokes'] += 1
-					data_dict['Total Inactive Pokes'] += 1
-			if ts > 3600 and ts <= 5400: #Inactive Block 3
-				if time_check >= 60:
-					data_dict['B3 Inactive Time-In Pokes'] += 1
-					data_dict['B3 Total Inactive Pokes'] += 1
-					data_dict['Inactive Time-In Pokes'] += 1
-					data_dict['Total Inactive Pokes'] += 1
-				if time_check < 60:
-					data_dict['B3 Inactive Time-Out Pokes'] += 1
-					data_dict['B3 Total Inactive Pokes'] += 1
-					data_dict['Inactive Time-Out Pokes'] += 1
-					data_dict['Total Inactive Pokes'] += 1
-			if ts > 5400: #Inactive Block 4
-				if time_check >= 60:
-					data_dict['B4 Inactive Time-In Pokes'] += 1
-					data_dict['B4 Total Inactive Pokes'] += 1
-					data_dict['Inactive Time-In Pokes'] += 1
-					data_dict['Total Inactive Pokes'] += 1
-				if time_check < 60:
-					data_dict['B4 Inactive Time-Out Pokes'] += 1
-					data_dict['B4 Total Inactive Pokes'] += 1
-					data_dict['Inactive Time-Out Pokes'] += 1
-					data_dict['Total Inactive Pokes'] += 1
+		#time_check = ts - temp_time
 		if event == 'Puff':
+			puff_time = ts
 			if ts <= 1800: #Puff Block 1
 				data_dict['B1 Puffs'] += 1
 				data_dict['Total Puffs'] += 1
@@ -126,6 +34,152 @@ if event_dict['Events'] != 'NaN':
 			if ts > 5400: #Puff Block 4
 				data_dict['B4 Puffs'] += 1
 				data_dict['Total Puffs'] += 1
+		if event == 'Active Poke':
+			if ts <= 1800: #Active Block 1
+				if puff_time != 'NaN':
+					time_check = ts - puff_time
+					if time_check >= 60:
+						data_dict['B1 Active Time-In Pokes'] += 1
+						data_dict['B1 Total Active Pokes'] += 1
+						data_dict['Active Time-In Pokes'] += 1
+						data_dict['Total Active Pokes'] += 1
+					if time_check < 60:
+						data_dict['B1 Active Time-Out Pokes'] += 1
+						data_dict['B1 Total Active Pokes'] += 1
+						data_dict['Active Time-Out Pokes'] += 1
+						data_dict['Total Active Pokes'] += 1
+				else:
+					data_dict['B1 Active Time-In Pokes'] += 1
+					data_dict['B1 Total Active Pokes'] += 1
+					data_dict['Active Time-In Pokes'] += 1
+					data_dict['Total Active Pokes'] += 1    
+			if ts > 1800 and ts <= 3600: #Active Block 2
+				if puff_time != 'NaN':
+					time_check = ts - puff_time
+					if time_check >= 60:
+						data_dict['B2 Active Time-In Pokes'] += 1
+						data_dict['B2 Total Active Pokes'] += 1
+						data_dict['Active Time-In Pokes'] += 1
+						data_dict['Total Active Pokes'] += 1
+					if time_check < 60:
+						data_dict['B2 Active Time-Out Pokes'] += 1
+						data_dict['B2 Total Active Pokes'] += 1
+						data_dict['Active Time-Out Pokes'] += 1
+						data_dict['Total Active Pokes'] += 1
+				else:
+					data_dict['B2 Active Time-In Pokes'] += 1
+					data_dict['B2 Total Active Pokes'] += 1
+					data_dict['Active Time-In Pokes'] += 1
+					data_dict['Total Active Pokes'] += 1 
+			if ts > 3600 and ts <= 5400: #Active Block 3
+				if puff_time != 'NaN':
+					time_check = ts - puff_time
+					if time_check >= 60:
+						data_dict['B3 Active Time-In Pokes'] += 1
+						data_dict['B3 Total Active Pokes'] += 1
+						data_dict['Active Time-In Pokes'] += 1
+						data_dict['Total Active Pokes'] += 1
+					if time_check < 60:
+						data_dict['B3 Active Time-Out Pokes'] += 1
+						data_dict['B3 Total Active Pokes'] += 1
+						data_dict['Active Time-Out Pokes'] += 1
+						data_dict['Total Active Pokes'] += 1
+				else:
+					data_dict['B3 Active Time-In Pokes'] += 1
+					data_dict['B3 Total Active Pokes'] += 1
+					data_dict['Active Time-In Pokes'] += 1
+					data_dict['Total Active Pokes'] += 1 
+			if ts > 5400: #Active Block 4
+				if puff_time != 'NaN':
+					time_check = ts - puff_time
+					if time_check >= 60:
+						data_dict['B4 Active Time-In Pokes'] += 1
+						data_dict['B4 Total Active Pokes'] += 1
+						data_dict['Active Time-In Pokes'] += 1
+						data_dict['Total Active Pokes'] += 1
+					if time_check < 60:
+						data_dict['B4 Active Time-Out Pokes'] += 1
+						data_dict['B4 Total Active Pokes'] += 1
+						data_dict['Active Time-Out Pokes'] += 1
+						data_dict['Total Active Pokes'] += 1
+				else:
+					data_dict['B4 Active Time-In Pokes'] += 1
+					data_dict['B4 Total Active Pokes'] += 1
+					data_dict['Active Time-In Pokes'] += 1
+					data_dict['Total Active Pokes'] += 1 
+		if event == 'Inactive Poke':
+			if ts <= 1800: #Inactive Block 1
+				if puff_time != 'NaN':
+					time_check = ts - puff_time
+					if time_check >= 60:
+						data_dict['B1 Inactive Time-In Pokes'] += 1
+						data_dict['B1 Total Inactive Pokes'] += 1
+						data_dict['Inactive Time-In Pokes'] += 1
+						data_dict['Total Inactive Pokes'] += 1
+					if time_check < 60:
+						data_dict['B1 Inactive Time-Out Pokes'] += 1
+						data_dict['B1 Total Inactive Pokes'] += 1
+						data_dict['Inactive Time-Out Pokes'] += 1
+						data_dict['Total Inactive Pokes'] += 1
+				else:
+					data_dict['B1 Inactive Time-In Pokes'] += 1
+					data_dict['B1 Total Inactive Pokes'] += 1
+					data_dict['Inactive Time-In Pokes'] += 1
+					data_dict['Total Inactive Pokes'] += 1 
+			if ts > 1800 and ts <= 3600: #Inactive Block 2
+				if puff_time != 'NaN':
+					time_check = ts - puff_time
+					if time_check >= 60:
+						data_dict['B2 Inactive Time-In Pokes'] += 1
+						data_dict['B2 Total Inactive Pokes'] += 1
+						data_dict['Inactive Time-In Pokes'] += 1
+						data_dict['Total Inactive Pokes'] += 1
+					if time_check < 60:
+						data_dict['B2 Inactive Time-Out Pokes'] += 1
+						data_dict['B2 Total Inactive Pokes'] += 1
+						data_dict['Inactive Time-Out Pokes'] += 1
+						data_dict['Total Inactive Pokes'] += 1
+				else:
+					data_dict['B2 Inactive Time-In Pokes'] += 1
+					data_dict['B2 Total Inactive Pokes'] += 1
+					data_dict['Inactive Time-In Pokes'] += 1
+					data_dict['Total Inactive Pokes'] += 1 
+			if ts > 3600 and ts <= 5400: #Inactive Block 3
+				if puff_time != 'NaN':
+					time_check = ts - puff_time
+					if time_check >= 60:
+						data_dict['B3 Inactive Time-In Pokes'] += 1
+						data_dict['B3 Total Inactive Pokes'] += 1
+						data_dict['Inactive Time-In Pokes'] += 1
+						data_dict['Total Inactive Pokes'] += 1
+					if time_check < 60:
+						data_dict['B3 Inactive Time-Out Pokes'] += 1
+						data_dict['B3 Total Inactive Pokes'] += 1
+						data_dict['Inactive Time-Out Pokes'] += 1
+						data_dict['Total Inactive Pokes'] += 1
+				else:
+					data_dict['B3 Inactive Time-In Pokes'] += 1
+					data_dict['B3 Total Inactive Pokes'] += 1
+					data_dict['Inactive Time-In Pokes'] += 1
+					data_dict['Total Inactive Pokes'] += 1 
+			if ts > 5400: #Inactive Block 4
+				if puff_time != 'NaN':
+					time_check = ts - puff_time
+					if time_check >= 60:
+						data_dict['B4 Inactive Time-In Pokes'] += 1
+						data_dict['B4 Total Inactive Pokes'] += 1
+						data_dict['Inactive Time-In Pokes'] += 1
+						data_dict['Total Inactive Pokes'] += 1
+					if time_check < 60:
+						data_dict['B4 Inactive Time-Out Pokes'] += 1
+						data_dict['B4 Total Inactive Pokes'] += 1
+						data_dict['Inactive Time-Out Pokes'] += 1
+						data_dict['Total Inactive Pokes'] += 1
+				else:
+					data_dict['B4 Inactive Time-In Pokes'] += 1
+					data_dict['B4 Total Inactive Pokes'] += 1
+					data_dict['Inactive Time-In Pokes'] += 1
+					data_dict['Total Inactive Pokes'] += 1
 
 #Calculate discrimination index
 if event_dict['Events'] != 'NaN':
